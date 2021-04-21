581940a3d1144b849261dcd48d67df0d2b1c219c
vertio <krzysztofdrzewiecki690@gmail.com>
2021-04-21 23:22:07 +0200

    Dodaj pliki projektów.

---
+++ AdvancedProgramming_Lesson2/Data/MvcContext.cs
@@ -0,0 +1,14 @@
+﻿using AdvancedProgramming_Lesson2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdvancedProgramming_Lesson2.Data
+{
+    public class MvcContext : DbContext
+    {
+        public MvcContext(DbContextOptions<MvcContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Localization ASP.NET Core Sample
+
+In order to perform clean database setup issue the following commands from 'Package Manager Console':
+
+```
+Drop-Database
+Update-Database
+
+```
+
+To add new resources modify file SharedResource.resx and corresponding SharedResource files responsible for separate languages.
+
+# Last Updated
+2021/04/17
